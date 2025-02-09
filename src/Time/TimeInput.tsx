26c7b5426aa70ca121edcf13b41e9c4210dfbd75
AnomalousLLC <85542084+AnomalousLLC@users.noreply.github.com>
2025-02-08 21:12:45 -0800

    Time Input - Properly align text

--- src/Time/TimeInput.tsx
+++ src/Time/TimeInput.tsx
@@ -104,7 +104,8 @@ function TimeInput(
                 : undefined,
             paddingTop: Platform.OS === 'android' ? 10 : undefined,
             paddingBottom: Platform.OS === 'android' ? 0 : undefined,
-            maxWidth: 96
+            maxWidth: 96,
+            textAlign: 'center'
           },
         ]}
         maxFontSizeMultiplier={1.5}
